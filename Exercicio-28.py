a82353836625ac5892efefd94669447e529a55f1
Souza-Juliano <89814162+Souza-Juliano@users.noreply.github.com>
2021-11-21 21:14:49 -0300

    Add files via upload

---
+++ Exercicio-28.py
@@ -0,0 +1,34 @@
+#28. Exibir a tabuada dos valores de um a vinte, no intervalo de um a dez. 
+# Entre as tabuadas, solicitar que o usuário pressione uma tecla.
+
+num = 0
+i = 1
+
+for num in range(0, 20, 1):
+    num = num+1
+    i = 1 
+    while(i < 11 ): 
+            
+          r = num * i
+          print(f'{num} X {i} = {r}')
+          i = i + 1
+
+
+
+    
+
+
+
+
+
+
+
+
+     
+    
+    
+         
+
+         
+    
+
