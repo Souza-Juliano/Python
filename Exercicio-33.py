10d344231c0878e4b2e4f16c2a02ce3fea7ff9b9
Souza-Juliano <89814162+Souza-Juliano@users.noreply.github.com>
2021-11-21 22:19:11 -0300

    Update Exercicio-33.py

--- Exercicio-33.py
+++ Exercicio-33.py
@@ -1,12 +1,12 @@
 #33. Armazenar dez números na memória do computador. Exibir os valores na ordem inversa à da digitação.
 numeros = []
 
-for i in range(0, 11, 1):
+for i in range(0, 10, 1):
     x = int(input('Digite um numero: '))
     numeros.append(x)
  
 print('Os numeros que você digitou foram: ')
  
 # Exibir de forma crescente
-for i in range(10, -1, -1):
+for i in range(9, -1, -1):
     print(numeros[i])
